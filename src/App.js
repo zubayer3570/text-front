596440508a864c2e346a6aa8c5a34bf62c7fb33e
zubayer <zubayer3570@gmail.com>
2022-04-30 06:16:32 +0600

    '1'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  const [texts, setTexts] = useState([])
+  const [reload, setReload] = useState({})
+  const handleSend = (e) => {
+    e.preventDefault()
+    const input = e.target.text.value
+    const text = { text: `---${input}` }
+    fetch('https://secret-springs-73355.herokuapp.com/text', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(text)
+    })
+      .then(res => res.json())
+      .then(data => {
+        setReload(data)
+      })
+  }
+  useEffect(() => {
+    fetch('https://secret-springs-73355.herokuapp.com/text')
+      .then(res => res.json())
+      .then(data => setTexts(data))
+  }, [reload])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Text</h1>
+      <div className="text-screen">
+        {
+          texts?.map(text => <h2 key={text._id}>{text.text}</h2>)
+        }
+      </div>
+      <form onSubmit={handleSend}>
+        <input type="text" name="text" className='input' />
+        <input type="submit" value="send" />
+      </form>
+
     </div>
   );
 }
